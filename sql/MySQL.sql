b92e7f2780a467eaf23a19599f817e013ae5c924
Anatoly Soldatov <soldatov.anatoly@gmail.com>
2022-10-24 19:11:59 +0400

    Update MySQL.sql

--- sql/MySQL.sql
+++ sql/MySQL.sql
@@ -13,6 +13,25 @@ WHERE LOWER(T.COLUMN_NAME) LIKE '%staff%'
 ORDER BY T.TABLE_SCHEMA, T.TABLE_NAME, T.COLUMN_NAME
 LIMIT 100;
 
--- Поиск процедуры или функции
+-- Поиск процедуры или функции по названию
 -- Поле ROUTINE_TYPE может быть PROCEDURE или FUNCTION.
 SELECT * FROM information_schema.ROUTINES WHERE SPECIFIC_NAME like '%proc_name%'
+
+-- 
+
+-- Описание таблицы с полями
+select
+concat(t.TABLE_SCHEMA, '.', t.`TABLE_NAME`, '
+', t.TABLE_COMMENT, '
+', (
+SELECT
+group_concat(concat(c.`COLUMN_NAME`, if(trim(c.COLUMN_COMMENT) = '', '', concat(' - ', c.COLUMN_COMMENT))) separator '
+')
+FROM
+  INFORMATION_SCHEMA.COLUMNS as c
+WHERE c.TABLE_SCHEMA = t.TABLE_SCHEMA and c.`TABLE_NAME` = t.`TABLE_NAME`
+), '
+')
+from information_schema.`TABLES` as t
+where t.TABLE_SCHEMA in ('database') AND t.`TABLE_NAME` in ('table')
+order by t.TABLE_SCHEMA, t.`TABLE_NAME`
